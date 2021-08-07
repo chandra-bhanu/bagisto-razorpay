d3e2b29084b6049fdc00be4963dd9e2af678074f
ChandraBhanu Das <chandra-bhanu@users.noreply.github.com>
2021-08-07 23:54:19 +0530

    Added Razorpay status and payment infos
    razorpay_payment_id and status from gateway

--- Neexpg/Razorpay/src/Http/Controllers/RazorpayController.php
+++ Neexpg/Razorpay/src/Http/Controllers/RazorpayController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace neexPg\Razorpay\Http\Controllers;
+namespace Neexpg\Razorpay\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Razorpay\Api\Api;
@@ -8,7 +8,7 @@ use Webkul\Sales\Repositories\InvoiceRepository;
 use Webkul\Sales\Repositories\OrderRepository;
 use Webkul\Checkout\Facades\Cart;
 use App\Exceptions\Handler;
-
+use Webkul\Sales\Models\OrderPayment as OrderPayment;
 
 class RazorpayController extends Controller
 {
@@ -75,26 +75,46 @@ class RazorpayController extends Controller
                 $pgorderId = $orderAPI['id']; // Get the created Order ID
                 
                 //CREATE FORM DATA AND PUSH TO VIEW   
-                $paymentData=array();
+                $payment=array();
                 $payment['apikey']=$merchentId;
                 $payment['oid']=$pgorderId;
                 $payment['name']=core()->getConfigData('sales.paymentmethods.razorpay.merchantname');
                 $payment['description']=core()->getConfigData('sales.paymentmethods.razorpay.merchantdesc');
                 $payment['mobile']= $cart->billing_address->phone;
-                $payment['email']=$cart->billing_address->email;
+                $payment['email']= $cart->billing_address->email;
                 $payment['address']=$cart->billing_address->city.','.$cart->billing_address->state.','.$cart->billing_address->postcode.','.$cart->billing_address->country;
                 
                 
-                
-                
+                $order = $this->orderRepository->create(Cart::prepareDataForOrder());
+                $this->order = $this->orderRepository->findOneWhere([
+                'cart_id' => Cart::getCart()->id
+                ]);
             
+                
+                $pgUpdateD = OrderPayment::where('order_id',$this->order->id)->firstOrFail();
+                $additional=array();
+                $additional['status']='Pending Payment';
+                $additional['oid']=$pgorderId;
+                $additional['pgreference']='';                
+                $pgUpdateD->additional=$additional;
+                $pgUpdateD->save();
+              //  $pgUpdateD->update(['additional->status' => 'Pending Payment','additional->oid' =>$pgorderId,'additional->pgreference' =>'']);
+              
+                
+                $this->orderRepository->update(['status' => 'pending_payment'], $this->order->id);
+                
+             
 
-            return view('razorpay::drop-in-ui', compact('paymentData');
-'));
+          return view('razorpay::drop-in-ui', compact('payment'));
+
             }
             catch(\Exception $e)
             {
-                return redirect()->back();
+                
+                echo 'Message: ' .$e->getMessage();
+                
+                
+               // return redirect()->back();
             }
         }
         else {
@@ -107,59 +127,7 @@ class RazorpayController extends Controller
      *
      * @return response
      */
-    public function transaction(Request $request)
-    {
-        if(core()->getConfigData('sales.paymentmethods.braintree.debug') == '1')
-            { $debug = 'sandbox'; }
-        else
-           { $debug = 'production'; }
-
-        $gateway = new Braintree_Gateway([
-            'environment' => $debug,
-            'merchantId' => core()->getConfigData('sales.paymentmethods.braintree.braintree_merchant_id'),
-            'publicKey' => core()->getConfigData('sales.paymentmethods.braintree.braintree_public_key'),
-            'privateKey' => core()->getConfigData('sales.paymentmethods.braintree.braintree_private_key')
-        ]);
-
-        $clientToken = $gateway->clientToken()->generate();
-
-        $cartAmount = \Cart::getCart()->base_grand_total;
-
-        $payload = $request->input('payload', false);
-        $nonceFromTheClient = $payload['nonce'];
-
-        $result = $gateway->transaction()->sale([
-            'amount' => $cartAmount,
-            'paymentMethodNonce' => $nonceFromTheClient,
-            'options' => [
-              'submitForSettlement' => True
-            ]
-          ]);
-
-        if($result->success == 'true') {
-
-            $order = $this->orderRepository->create(Cart::prepareDataForOrder());
-
-            $this->order = $this->orderRepository->findOneWhere([
-                'cart_id' => Cart::getCart()->id
-                ]);
-
-            $this->orderRepository->update(['status' => 'processing'], $this->order->id);
-
-            Cart::deActivateCart();
-
-            session()->flash('order', $order);
-
-            $this->invoiceRepository->create($this->prepareInvoiceData());
-
-            session()->flash('success', trans('braintree::app.payment-successfull'));
-
-        }
-        // return redirect()->route('shop.checkout.success');
-        return response()->json($result);
-
-    }
-
+   
     public function verifyPaymentHook(Request $request)
     {
           if(core()->getConfigData('sales.paymentmethods.razorpay.sandbox')):
@@ -179,19 +147,37 @@ class RazorpayController extends Controller
         $generatedSignature = hash_hmac('sha256',$request->getContent(), $privateKey);
         if ($generatedSignature == $webhookSignature):
             
+            //verify agnaist payment
+            
         endif;
     }
     
      //PAYMENT CANCELLED
-    public function cancelOrder(Request $request)
+    public function paymentFail(Request $request)
     {
+        //$order = $this->orderRepository->create(Cart::prepareDataForOrder());
+                $this->order = $this->orderRepository->findOneWhere([
+                'cart_id' => Cart::getCart()->id
+                ]);
+            
+                $pgUpdateD = OrderPayment::where('order_id',$this->order->id)->firstOrFail();
+                $additional=array();
+                $additional['status']='Canceled Payment';
+                $pgUpdateD->additional=$additional;
+                $pgUpdateD->save();
+                
+                
+                
+                $this->orderRepository->update(['status' => 'canceled'], $this->order->id);
+        Cart::deActivateCart();
+         session()->flash('danger','Order Cancelled!');
         //CANCELL ORDER
          return redirect()->route('shop.checkout.fail')->with('errormessage', 'Order Cancelled!');
     }
     
     
     //PAYMENT SUCEESS
-    public function confirmOrder(Request $request)
+    public function paymentSuccess(Request $request)
     {
          if(core()->getConfigData('sales.paymentmethods.razorpay.sandbox')):
             $merchentId = core()->getConfigData('sales.paymentmethods.razorpay.testclientid');
@@ -204,8 +190,26 @@ class RazorpayController extends Controller
         
         
         if(isset($request['error'])):
+           // $order = $this->orderRepository->create(Cart::prepareDataForOrder());
+                $this->order = $this->orderRepository->findOneWhere([
+                'cart_id' => Cart::getCart()->id
+                ]);
+            
+                $pgUpdateD = OrderPayment::where('order_id',$this->order->id)->firstOrFail();
+                $additional=array();
+                $additional['status']=$request['error']['description'];
+                $pgUpdateD->additional=$additional;
+                $pgUpdateD->save();
+        
+        
+        
+                $this->orderRepository->update(['status' => 'pending_payment'], $this->order->id);
+            Cart::deActivateCart();
+            session()->flash('danger',$request['error']['description']);
             return redirect()->route('shop.checkout.fail')->with('errormessage', $request['error']['description']);
         else:
+            
+            
             $oid =  $request['razorpay_order_id'];
             $api = new Api($merchentId,$privateKey);
             $expected_signature = $request['razorpay_order_id']. '|' .$request['razorpay_payment_id'] ;
@@ -213,8 +217,25 @@ class RazorpayController extends Controller
             if ($generatedSignature == $request['razorpay_signature']):
                 
                 //PROCESS THE ORDER
-                
-                
+               // $order = $this->orderRepository->create(Cart::prepareDataForOrder());
+                $this->order = $this->orderRepository->findOneWhere([
+                'cart_id' => Cart::getCart()->id
+                ]);
+            
+               $pgUpdateD = OrderPayment::where('order_id',$this->order->id)->firstOrFail();
+                $additional=array();
+                $additional['status']='Paid';
+                $additional['oid']=$request['razorpay_order_id'];
+                $additional['pgreference']=$request['razorpay_payment_id'];                
+                $pgUpdateD->additional=$additional;
+                $pgUpdateD->save();
+            
+                $this->orderRepository->update(['status' => 'processing'], $this->order->id);
+                $this->invoiceRepository->create($this->prepareInvoiceData());
+
+                Cart::deActivateCart();
+                session()->flash('order', $this->order);                
+                session()->flash('success', trans('Payment Successfull!'));
                 return redirect()->route('shop.checkout.success');
             else:
                return redirect()->route('shop.checkout.fail')->with('errormessage', 'Something is not right, for security reason the transaction can\'t be processed.'); 
